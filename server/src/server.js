dcb30483e8f93cc6c521ee746ea0f979c724feb5
safarhana <farhana.mim080@gmail.com>
2026-02-06 13:36:04 +0600

    morgan and nodemon package installed

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('dev'));
+
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: 'api is working fine!',
+  });
+});
+
+app.listen(3001, () => {
+  console.log(`Server is running on port http://localhost:3001`);
+});
