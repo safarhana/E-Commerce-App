72b2cdab7993a51d0a0925b759f19d9e26f720ae
safarhana <farhana.mim080@gmail.com>
2026-02-19 13:53:33 +0600

    some changes

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,23 @@ const app = express();
 
 app.use(morgan('dev'));
 
+const isLoggedIn = (req, res, next) => {
+  console.log("isLoggedIn middleware called");
+  next();
+};
 
 app.get("/test", (req, res) => {
   res.status(200).send({
-    message: 'api is working fine!',
+    message: 'api testing is working fine!',
   });
 });
+
+app.get("/api/user",isLoggedIn, (req, res) => {
+  res.status(200).send({
+    message: 'user profile is returned!',
+  });
+});
+
 
 app.listen(3001, () => {
   console.log(`Server is running on port http://localhost:3001`);
